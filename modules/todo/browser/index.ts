f06e2d0703408db00df01c1fb096f28e17f86052
Hunter <273676636@qq.com>
2022-07-27 16:09:17 +0800

    add toto module

---
+++ modules/todo/browser/index.ts
@@ -0,0 +1,8 @@
+import { Provider, Injectable } from '@opensumi/di';
+import { BrowserModule } from '@opensumi/ide-core-browser';
+import { TodoContribution } from './todo.contribution';
+
+@Injectable()
+export class TodoListModule extends BrowserModule {
+  providers: Provider[] = [TodoContribution];
+}
